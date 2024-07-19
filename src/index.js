289bca80f70fe0a803813b68a21ceef5c46cf12f
Dave J <29786887+daveswork@users.noreply.github.com>
2024-07-18 22:33:18 -0400

    Tested adding items.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const formIntercept = document.querySelector("#new-task-description");
+  const todoList = document.querySelector("#tasks");
+  const todoForm = document.querySelector("#create-task-form");
+
+  todoForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const listItemText = e.target["new-task-description"].value;
+    addListItem(listItemText, todoList);
+  })
 });
+
+function addListItem(someText, listName){
+  let listElement = document.createElement("li");
+  listElement.textContent = someText;
+  listName.appendChild(listElement);
+
+}
